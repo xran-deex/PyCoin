831b48fe7e67cc0f0885af01b1ada10424bfbabe
Randy Valis <randy.valis@gmail.com>
2014-10-29 21:08:27 -0400

    Client GUI | Created an empty user interface component

---
+++ GUI/gui.py
@@ -0,0 +1,16 @@
+''' The main user interface component for PyCoin '''
+
+class GUI:
+	
+	''' Sets up the ui '''
+	def setup():
+		print('Setting up the ui...')
+
+	''' Displays the user interface??? '''
+	def show():
+		setup()
+		print('Showing the ui to the user...')
+
+if __name__ == '__main__':
+	gui = GUI()
+	gui.show()
