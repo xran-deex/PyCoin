459a7c18ef2b33c65c79d89a83ac50bee3165282
Chima Nwanna <mariox2099@gmail.com>
2014-11-04 02:44:12 -0500

    Early GUI and stuff

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,16 +1,55 @@
-''' The main user interface component for PyCoin '''
+from Tkinter import *
+from ttk import Button, Style
 
-class GUI:
-	
-	''' Sets up the ui '''
-	def setup(self):
-		print('Setting up the ui...')
+class GUI(Frame):
+    
+    def __init__(self, parent):
+        Frame.__init__(self, parent) 
+        self.parent = parent
+        self.setup()
+    
+    def setup(self):
+      
+        self.parent.title("PyCoin")
+        self.pack(fill=BOTH, expand=1)
 
-	''' Displays the user interface??? '''
-	def show(self):
-		self.setup()
-		print('Showing the ui to the user...')
+        #Label and Text field for BitCoin Amount
+        self.label = Label(self, text="Amount of Bitcoins to send:")
+        self.label.grid(column=0, row=0)
+        self.label.pack()
+        
+        self.amountBox = Entry(self, bd=2)
+        self.amountBox.pack(fill=X, pady=10)
+
+        #Label and Text filed for Reciever Key
+        self.label = Label(self, text="Recievers Public Key:")
+        self.label.grid(column=0, row=0)
+        self.label.pack()
+
+        self.recieverKey = Text(self, width=40, height=15)
+        self.recieverKey.grid(column=0, row=2)
+        self.recieverKey.pack()
+
+        #Send Button
+        self.sendBtn = Button(self, text='\nSend BitCoin(s)\n', command=self.sendCoins)
+        self.sendBtn.grid(column=0, row=2)
+        self.sendBtn.pack(fill=X, pady=10)
+
+        #Status Bar 
+        status = Label(self, text="Ready", bd=1, relief=SUNKEN, anchor=W)
+        status.pack(side=BOTTOM, fill=X)
+
+    #Send Button Callback Function  
+    def sendCoins(self):
+        print 'Sent {} BitCoins to reciever: {}'.format(self.amountBox.get(), self.recieverKey.get("1.0",END))
+
+def main():
+  
+    root = Tk()
+    
+    app = GUI(root)
+    root.mainloop()  
+
 
 if __name__ == '__main__':
-	gui = GUI()
-	gui.show()
+    main()  
