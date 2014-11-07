c1d13694093ad33d35a9de4139c60db23db5a403
Randy Valis <randy.valis@gmail.com>
2014-11-06 21:05:55 -0500

    Revert to old transaction_test file

--- tests/transaction_tests.py
+++ tests/transaction_tests.py
@@ -13,41 +13,32 @@ class TestTransactionClass(unittest.TestCase):
   def setUp(self):
     self.key = KeyStore.getPublicKey()
     self.trans = Transaction()
-    print('starting test...')
     
-  def tearDown(self):
-    print('ending test...')
+  def test_create_transaction(self):
     
-  def test_dummy_test(self):
-    print('nothing to do')
+    self.trans.add_input(Transaction.Input(20, b'FFFFFFFF'))
+    self.trans.add_output(Transaction.Output(10, self.key.publickey())) # just pay to ourselves for now
+    self.trans.add_input(Transaction.Input(5, None))
     
-    self.assertTrue('This is true')
+    # verify the transaction
+    message = SHA256.new(str.encode('signature'))
+    verifier = PKCS1_v1_5.new(self.trans.output[0].pubKey)
     
-  # def test_create_transaction(self):
+    verified = verifier.verify(message, self.trans.input[0].signature)
+    self.assertTrue(verified)
     
-  #   self.trans.add_input(Transaction.Input(20, b'FFFFFFFF'))
-  #   self.trans.add_output(Transaction.Output(10, self.key.publickey())) # just pay to ourselves for now
-  #   self.trans.add_input(Transaction.Input(5, None))
+  def test_build_raw_transaction(self):
     
-  #   # verify the transaction
-  #   message = SHA256.new(str.encode('signature'))
-  #   verifier = PKCS1_v1_5.new(self.trans.output[0].pubKey)
-    
-  #   verified = verifier.verify(message, self.trans.input[0].signature)
-  #   self.assertTrue(verified)
-    
-  # def test_build_raw_transaction(self):
-    
-  #   self.trans.add_input(Transaction.Input(20, b'FFFFFFFF'))
-  #   self.trans.add_output(Transaction.Output(10, self.key.publickey())) # just pay to ourselves for now
-  #   self.trans.add_input(Transaction.Input(5, b'FFFFFFFF'))
-  #   s = self.trans.build_struct()
-  #   c = P2PClient()
-  #   c.send_message(Message.add)
-  #   c = P2PClient()
-  #   c.send_message(Message.new_transaction, s)
-  #   self.trans.unpack(s)
-  #   self.assertIsNotNone(s)
+    self.trans.add_input(Transaction.Input(20, b'FFFFFFFF'))
+    self.trans.add_output(Transaction.Output(10, self.key.publickey())) # just pay to ourselves for now
+    self.trans.add_input(Transaction.Input(5, b'FFFFFFFF'))
+    s = self.trans.build_struct()
+    c = P2PClient()
+    c.send_message(Message.add)
+    c = P2PClient()
+    c.send_message(Message.new_transaction, s)
+    self.trans.unpack(s)
+    self.assertIsNotNone(s)
     
 if __name__ == '__main__':
   unittest.main()
