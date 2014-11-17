a610cf3c2bcbc4349b7e8a5a0e4ecd97583b0272
Chima Nwanna <mariox2099@gmail.com>
2014-11-17 16:14:46 -0500

    Added a balance indicator, confirmation box for sending coins, and catches for empty test fields

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,57 +1,97 @@
 from tkinter import *
-<<<<<<< HEAD
-#from ttk import Button, Style
-=======
 from tkinter.ttk import Button, Style
->>>>>>> 05bad0c8ab16cbe02f9fc6b143139e401dcb2b35
 
-class GUI(Frame):
+
+class PyCoin(Frame):
+
+    coin_balance = 0
     
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.parent = parent
-        self.setup()
+        self.initApp()
+        self.setupGUI()
+
+    def initApp(self):
+        #Connect Here
+
+        #Get balance, Save to variable below
+        PyCoin.coin_balance = 983
+        
+        print("App initiated")
     
-    def setup(self):
+    def setupGUI(self):
       
         self.parent.title("PyCoin")
         self.pack(fill=BOTH, expand=1)
 
-        #Label and Text field for BitCoin Amount
-        self.label = Label(self, text="Amount of Bitcoins to send:")
-        #self.label.grid(column=0, row=0)
-        self.label.pack()
+        #Labeled Frames for Status and Send
+        lf = LabelFrame(self, text="Send BitCoins")
+        lf.pack(side=BOTTOM, fill="both", expand="yes", padx=7, pady=7)
+
+        lf2 = LabelFrame(self, text="BitCoin Status")
+        lf2.pack(side=TOP, fill="both", expand="yes", padx=7, pady=7)
         
-        self.amountBox = Entry(self, bd=2)
-        self.amountBox.pack(fill=X, pady=10)
+        #Labels for Bitcoin balance
+        self.balance = Label(lf2, text="BitCoin Balance:\n" + str(PyCoin.coin_balance),
+                             font = "Helvetica 20")
+        self.balance.pack()
+        
+        
+        #Label and Text field for BitCoin Amount
+
+        self.amtFrame = Frame(lf);
+        self.amtFrame.pack()
+        
+        self.label = Label(self.amtFrame, text="BitCoin Amount :")
+        self.label.pack(side=LEFT)
+        
+        self.amountBox = Entry(self.amtFrame, bd=2)
+        self.amountBox.pack(side=LEFT, pady=10, fill=X)
+        
 
         #Label and Text filed for Reciever Key
-        self.label = Label(self, text="Recievers Public Key:")
-        #self.label.grid(column=0, row=0)
+        self.label = Label(lf, text="Receivers Public Key:")
         self.label.pack()
 
-        self.recieverKey = Text(self, width=40, height=15)
-        #self.recieverKey.grid(column=0, row=2)
+        self.recieverKey = Text(lf, width=30, height=10)
         self.recieverKey.pack()
 
         #Send Button
-        self.sendBtn = Button(self, text='\nSend BitCoin(s)\n', command=self.sendCoins)
-        #self.sendBtn.grid(column=0, row=2)
-        self.sendBtn.pack(fill=X, pady=10)
+        self.sendBtn = Button(lf, text='\nSend BitCoin(s)\n', command=self.sendCoins)
+        self.sendBtn.pack(fill=X, pady=5)
 
         #Status Bar
-        status = Label(self, text="Ready", bd=1, relief=SUNKEN, anchor=W)
-        status.pack(side=BOTTOM, fill=X)
+        #status = Label(lf, text="Ready", bd=1, relief=SUNKEN, anchor=W)
+        #status.pack(side=BOTTOM, fill=X)
 
+        print("GUI Initiated")
+
+    
     #Send Button Callback Function
     def sendCoins(self):
-        print('Sent {} BitCoins to reciever: {}'.format(self.amountBox.get(), self.recieverKey.get("1.0",END)))
+        sendAmt = self.amountBox.get()      	    #Amount to send
+        recvKey = self.recieverKey.get("1.0",END)   #recievers public key
+        
+        if not sendAmt:
+            messagebox.showwarning("Warning", "Please enter a BitCoin amount.")
+        elif len(recvKey) <= 1:
+            messagebox.showwarning("Warning", "Please enter the receivers key.")
+        else:
+            result = messagebox.askyesno("Send Confirmation", 'Sending {} BitCoins to reciever:\n {}'.format(sendAmt, recvKey))
 
+            if result == True:
+                 print('Sending {} BitCoins to reciever:\n {}'.format(sendAmt, recvKey))
+            else:
+                None
+            self.amountBox.delete(0,END)
+           
+        
 def main():
   
     root = Tk()
+    app = PyCoin(root)
     
-    app = GUI(root)
     root.mainloop()
 
 
